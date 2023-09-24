f7083cb124b9056586c75be8c4175d102b23dc42
Ivan Veloz <devel@ivanveloz.com>
2023-09-24 15:30:31 -0400

    Updated for python3, dropped support for python2

--- python/example-servo.py
+++ python/example-servo.py
@@ -5,16 +5,18 @@ import time
 import virtGPIO as GPIO
 
 myServo = GPIO.Servo(7)   # servo on pin 7
-c=0L
+c=0
 
 while True:
-    print c
+    print(c)
     c += 5
-    c = c & 0x7f  # limit to 127
+
+    if (c>127):  # limit to 127
+        c=0
 
     myServo.write(c)   # sweeps slowly 0 - 127 degrees then returns
 
-    time.sleep(1)
+    time.sleep(2)
 
     myServo.stop()    # detaches (You DONT have to do it this way)
 
